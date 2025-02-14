c364f4952aa639d937c1f1f1e4f56dd9d6c2a70f
Steve <steve@Steves-MacBook-Pro.local>
2025-02-14 00:20:55 -0500

    tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import path from "path"
 import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
 import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
 
