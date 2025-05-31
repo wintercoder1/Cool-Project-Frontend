46170a0b49fd5999dd4709e5e2aa612944b518a6
Steve <steve@Steves-MBP.attlocal.net>
2025-05-31 01:54:29 -0400

    Enabled new company query on financial contributions..

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -422,16 +422,16 @@ const MainPage = () => {
         
 
         {/* New company/individual response button */}
-        {category !== 'Financial Contributions' && (
-          <div className="fixed bottom-6 right-8">
-            <button 
-              className="w-14 h-12 bg-blue-400 rounded-full flex items-center justify-center text-white shadow-lg hover:bg-blue-600 transition-colors"
-              onClick={(event) => handleNewQueryClick(event)}
-            >
-              <Plus size={50} />
-            </button>
-          </div>
-        )}
+        {/* {category !== 'Financial Contributions' && ( */}
+        <div className="fixed bottom-6 right-8">
+          <button 
+            className="w-14 h-12 bg-blue-400 rounded-full flex items-center justify-center text-white shadow-lg hover:bg-blue-600 transition-colors"
+            onClick={(event) => handleNewQueryClick(event)}
+          >
+            <Plus size={50} />
+          </button>
+        </div>
+        {/* )} */}
 
       </div>
     </div>
