4004626eecaf83f6e25b161cc73a18fe7956c957
Steve <steve@Steves-MacBook-Pro.local>
2025-06-10 17:17:29 -0400

    changed logo text margin.

--- src/components/LogoHeader.jsx
+++ src/components/LogoHeader.jsx
@@ -10,7 +10,7 @@ export default function LogoHeader({ onClick, className = "" }) {
       tabIndex={onClick ? 0 : undefined}
       style={{ cursor: onClick ? 'pointer' : 'default' }}
     >
-      <img src={tilt_logo} className="block" width="55" height="55" alt="blue_check_logo" />
+      <img src={tilt_logo} className="block" width="55" height="55" alt="tilt_logo" />
       <h1 className="text-4xl font-bold text-black">Tilt AI       </h1>
     </div>
   );
