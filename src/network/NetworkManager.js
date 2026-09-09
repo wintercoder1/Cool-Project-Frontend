910f9ed18feb2fa8ada372611d828dc9e661aaf9
Steve <wintercoder1@gmail.com>
2026-09-09 00:49:27 -0400

    With fix to point to correct prod backend.

--- src/network/NetworkManager.js
+++ src/network/NetworkManager.js
@@ -1,7 +1,7 @@
 class NetworkManager {
   constructor() {
-    // this.baseURL = import.meta.env.VITE_BASE_URL || 'http://127.0.0.1:8000';
-    this.baseURL = 'http://127.0.0.1:8000';
+    this.baseURL = import.meta.env.VITE_BASE_URL || 'http://127.0.0.1:8000';
+    // this.baseURL = 'http://127.0.0.1:8000';
 
     this.defaultOptions = {
       headers: {
